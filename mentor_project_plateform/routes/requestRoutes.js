d8117e9c985f4a12b0131772b98e2f57f09a30dc
Florent20 <louisflorentbwam@gmail.com>
2026-08-12 18:31:00 +0100

    request routes

---
+++ mentor_project_plateform/routes/requestRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {
+  sendRequest,
+  getAllRequests,
+  getRequestsByMentor,
+  getRequestsByMentee,
+  respondToRequest,
+  getActiveRelationships
+} = require('../controllers/requestController');
+
+router.post('/', sendRequest);
+router.get('/', getAllRequests);
+router.get('/active', getActiveRelationships);
+router.get('/mentor/:mentorId', getRequestsByMentor);
+router.get('/mentee/:menteeId', getRequestsByMentee);
+router.put('/:id/respond', respondToRequest);
+
+module.exports = router;
