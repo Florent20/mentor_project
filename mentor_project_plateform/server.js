47fb37ab9632d350dc5c7985c48c82a775f562f4
Florent20 <louisflorentbwam@gmail.com>
2026-08-12 18:20:17 +0100

    connect routes

--- mentor_project_plateform/server.js
+++ mentor_project_plateform/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
+const mentorRoutes = require('./routes/mentorRoutes');
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+app.use('/api/mentors', mentorRoutes);
 
 app.get('/', (req, res) => {
   res.send('Mentor-Mentee Platform API is running');
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
